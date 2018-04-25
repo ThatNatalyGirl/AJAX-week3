a5a728f109813448e9ecfa42a9afecf2650ff088
Nataly <ncgarzon88@gmail.com>
2018-04-25 17:36:52 -0400

    Sass updates

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,38 @@
 "use strict";
 
-console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+console.log("Piano");
+
+// Just has an API key that you have to pass along. 
+// This is a simple API
+// Social networks have a lot more security and are a lot more complicated
+
+// http://developer.nytimes.com/
+
+// You're requesting a simple API key (because it's not sensitive) 
+
+// Everyone has their own api key
+// You can only get 1000 users on your website a day so they have specifications through the site. 
+
+// Make sure you're using the correct terms of use
+
+// Scroll down - Article Search 
+// q  string
+// Location: query ?q=xyz
+
+// Search query term. Search is performed on the article body, headline and byline.
+
+// When we make that call'
+
+
+// you need a for loop to go through each of the results pull the the elements through the dom like .data.x.y..... (title thumbnail url)
+
+// add it to the dom in a div or whatever
+
+// q is an expected peramiter for the API 
+
+// const searchTerm = searchEl.value: 
+
+// www.nyt.com...your API....+ "?q=" + searchTerm
+
+// We are only Geting data not posting
 //# sourceMappingURL=main.js.map
