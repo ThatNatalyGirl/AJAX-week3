2bafe25a92a03e5d0ab1a6eb1a354a08fd641a04
Nataly <ncgarzon88@gmail.com>
2018-04-25 23:08:43 -0400

    Starting Ajax process

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,23 +1,42 @@
-"use strict";
+'use strict';
 
-console.log("Piano");
+console.log('Piano');
 
-// Just has an API key that you have to pass along. 
-// This is a simple API
-// Social networks have a lot more security and are a lot more complicated
+var searchBtn = document.getElementById('search');
+var articlesDiv = document.querySelector('.articles');
+var keywordValue = document.getElementById('keyword').value;
 
-// http://developer.nytimes.com/
+searchBtn.addEventListener("click", function (e) {
+	e.preventDefault();
+	getValueFromApi();
+});
 
-// You're requesting a simple API key (because it's not sensitive) 
+function getValueFromApi(keyword) {
+	axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json" + "4b5597618fc046b1a6045d770f61cc8b" + "?q=" + keywordValue).then(function (response) {
+		console.log('results?');
+	}).catch(function (error) {
+		console.warn('.axios encountered an error!', error);
+		valueEl.value = "UNDEFINED";
+	});
+}
 
-// Everyone has their own api key
-// You can only get 1000 users on your website a day so they have specifications through the site. 
+// for (var i = Things.length - 1; i >= 0; i--) {
+// 	Things[i]
+// }
 
-// Make sure you're using the correct terms of use
 
-// Scroll down - Article Search 
-// q  string
-// Location: query ?q=xyz
+// 	let getValueFromApi = function(searchTerm) {
+// 	axios.get(url)
+// 	.then(function (results) {
+
+// 		console.log("here is the get response data for key:", results.data,
+// 			results);
+// 		results.data.forEach ((result) => {
+// 			resultsDiv = document.createElement("div");
+// 			resultsDiv.innerHTML = result.data;
+// 		});
+
+// 	});
 
 // Search query term. Search is performed on the article body, headline and byline.
 
@@ -35,4 +54,22 @@ console.log("Piano");
 // www.nyt.com...your API....+ "?q=" + searchTerm
 
 // We are only Geting data not posting
+
+
+// Just has an API key that you have to pass along. 
+// This is a simple API
+// Social networks have a lot more security and are a lot more complicated
+
+// http://developer.nytimes.com/
+
+// You're requesting a simple API key (because it's not sensitive) 
+
+// Everyone has their own api key
+// You can only get 1000 users on your website a day so they have specifications through the site. 
+
+// Make sure you're using the correct terms of use
+
+// Scroll down - Article Search 
+// q  string
+// Location: query ?q=xyz
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,63 @@
 console.log(`Piano`)
 
+const searchBtn = document.getElementById('search');
+const articlesDiv = document.querySelector('.articles')
+const keywordValue = document.getElementById('keyword').value
+
+searchBtn.addEventListener("click", function(e) {
+			e.preventDefault();
+			getValueFromApi();
+		})
+
+function getValueFromApi(keyword){
+	axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json" + "4b5597618fc046b1a6045d770f61cc8b" + "?q=" + keywordValue)
+	.then(function(response){
+		console.log('results?')
+	})
+
+	.catch(function(error) {
+				console.warn('.axios encountered an error!', error);
+				valueEl.value = "UNDEFINED"
+			});
+}
+
+
+// for (var i = Things.length - 1; i >= 0; i--) {
+// 	Things[i]
+// }
+
+	
+
+// 	let getValueFromApi = function(searchTerm) {
+// 	axios.get(url)
+// 	.then(function (results) {
+
+// 		console.log("here is the get response data for key:", results.data,
+// 			results);
+// 		results.data.forEach ((result) => {
+// 			resultsDiv = document.createElement("div");
+// 			resultsDiv.innerHTML = result.data;
+// 		});
+		
+// 	});
+
+// Search query term. Search is performed on the article body, headline and byline.
+
+// When we make that call'
+
+
+// you need a for loop to go through each of the results pull the the elements through the dom like .data.x.y..... (title thumbnail url)
+
+// add it to the dom in a div or whatever
+
+// q is an expected peramiter for the API 
+
+// const searchTerm = searchEl.value: 
+
+// www.nyt.com...your API....+ "?q=" + searchTerm
+
+// We are only Geting data not posting
+
 
 // Just has an API key that you have to pass along. 
 // This is a simple API
@@ -18,20 +76,4 @@ console.log(`Piano`)
 // q  string
 // Location: query ?q=xyz
 
-// Search query term. Search is performed on the article body, headline and byline.
-
-// When we make that call'
-
-
-// you need a for loop to go through each of the results pull the the elements through the dom like .data.x.y..... (title thumbnail url)
-
-// add it to the dom in a div or whatever
-
-// q is an expected peramiter for the API 
-
-// const searchTerm = searchEl.value: 
-
-// www.nyt.com...your API....+ "?q=" + searchTerm
-
-// We are only Geting data not posting
 
